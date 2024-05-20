90812f21d26a15fe1c1a8a3aa2219c8735dc2fd2
Christopher Song <chris4728.song2001@gmail.com>
2024-05-19 23:23:17 -0500

    Initial FE for subscriptions, user auth

--- dough_divider_client/src/App.js
+++ dough_divider_client/src/App.js
@@ -1,27 +1,300 @@
 import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-import { gql, useQuery, useSubscription } from "@apollo/client";
+import { gql, useQuery, useMutation, useSubscription } from "@apollo/client";
 
-const TEST_QUERY = gql`
- {hello}
+const ADD_TRANSACTION = gql`
+  mutation add(
+    $leader: String!
+    $member: String!
+    $amount: Float!
+    $completed: Boolean!
+    $note: String!
+    $card: String!
+  ) {
+    addTransaction(
+      input: {
+        leader: $leader
+        member: $member
+        amount: $amount
+        completed: $completed
+        note: $note
+        card: $card
+      }
+    ) {
+      transaction {
+        transactionId
+        leader
+        member
+        amount
+        completed
+        note
+        card
+      }
+    }
+  }
 `;
 
-const COMMENTS_SUBSCRIPTION = gql`
-subscription {
-  counter
-}
+const UPDATE_TRANSACTION = gql`
+  mutation update($transactionId: ID!, $completed: Boolean!, $card: String!) {
+    updateTransaction(
+      transactionId: $transactionId
+      input: { completed: $completed, card: $card }
+    ) {
+      transaction {
+        leader
+        card
+        completed
+        member
+        amount
+        note
+      }
+    }
+  }
 `;
+
+const LEADER_SUBSCRIPTION = gql`
+  subscription leader($leader: String!) {
+    getTransactionByLeader(leader: $leader) {
+      transactionId
+      leader
+      member
+      amount
+      completed
+      note
+      card
+    }
+  }
+`;
+
+const MEMBER_SUBSCRIPTION = gql`
+  subscription member($member: String!) {
+    getTransactionByMember(member: $member) {
+      transactionId
+      leader
+      member
+      amount
+      completed
+      note
+      card
+    }
+  }
+`;
+
+const TEST_LEADER_ID = "Group01Leader";
+const TEST_MEMBER_ID_ONE = "User01";
+const TEST_MEMBER_ID_TWO = "User02";
 
 const App = () => {
-  // const { data, loading } = useQuery(TEST_QUERY);
-  const { data, loading } = useSubscription(COMMENTS_SUBSCRIPTION);
+  const [payments, setPayments] = useState([]);
+  const [memberLookupInPayments, setMemberLookup] = useState({});
+  const [numPaymentsComplete, setNumPaymentsComplete] = useState(1);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const [
+    addTransaction,
+    { data: dataAdd, loading: loadingAdd, error: errorAdd },
+  ] = useMutation(ADD_TRANSACTION, {
+    onCompleted: (result) => {
+      setPayments([...payments, result.addTransaction.transaction]);
 
-  return <div>Data is: {data.counter}</div>;
+      const member = result.addTransaction.transaction["member"];
+      const n = payments.length;
+
+      let newMemberLookup = { ...memberLookupInPayments };
+      newMemberLookup[member] = n;
+
+      setMemberLookup(newMemberLookup);
+    },
+  });
+
+  const [
+    updateTransaction,
+    { data: dataUpdate, loading: loadingUpdate, error: errorUpdate },
+  ] = useMutation(UPDATE_TRANSACTION);
+
+  // const { data: transactions, loading: loadingTransactions } = useQuery(
+  //   GET_ALL_TRANSACTIONS,
+  //   {
+  //     onCompleted: (result) => {
+  //       const transactions = result.getAllTransactions;
+  //       setPayments(transactions);
+  //     },
+  //   }
+  // );
+
+  const { data: dataLeader, loading: loadingLeader } = useSubscription(
+    LEADER_SUBSCRIPTION,
+    {
+      variables: { leader: TEST_LEADER_ID },
+      onData: (result) => {
+        const updatedTransaction = result.data.data.getTransactionByLeader;
+        const updatedPaymentIndex =
+          memberLookupInPayments[updatedTransaction["member"]];
+
+        let paymentsUpdated = [...payments];
+        let newPayment = { ...paymentsUpdated[updatedPaymentIndex] };
+
+        if (!newPayment.completed && updatedTransaction["completed"]) {
+          setNumPaymentsComplete(numPaymentsComplete + 1);
+        }
+
+        newPayment.completed = updatedTransaction["completed"];
+        newPayment.card = updatedTransaction["card"];
+        paymentsUpdated[updatedPaymentIndex] = newPayment;
+        setPayments(paymentsUpdated);
+      },
+    }
+  );
+
+  const { data: dataMember1, loading: loadingMember1 } = useSubscription(
+    MEMBER_SUBSCRIPTION,
+    {
+      variables: { member: TEST_MEMBER_ID_ONE },
+    }
+  );
+
+  const { data: dataMember2, loading: loadingMember2 } = useSubscription(
+    MEMBER_SUBSCRIPTION,
+    {
+      variables: { member: TEST_MEMBER_ID_TWO },
+    }
+  );
+
+  const initTransaction = () => {
+    addTransaction({
+      variables: {
+        leader: TEST_LEADER_ID,
+        member: TEST_LEADER_ID,
+        amount: 15,
+        completed: true,
+        card: "xyz000",
+        note: "Sample Note",
+      },
+    });
+  };
+
+  const addMemberOne = () => {
+    addTransaction({
+      variables: {
+        leader: TEST_LEADER_ID,
+        member: TEST_MEMBER_ID_ONE,
+        amount: 10,
+        completed: false,
+        card: "N/A",
+        note: "Sample Note",
+      },
+    });
+  };
+
+  const addMemberTwo = () => {
+    addTransaction({
+      variables: {
+        leader: TEST_LEADER_ID,
+        member: TEST_MEMBER_ID_TWO,
+        amount: 20,
+        completed: false,
+        card: "N/A",
+        note: "Sample Note",
+      },
+    });
+  };
+
+  const memberOneAccept = () => {
+    updateTransaction({
+      variables: {
+        transactionId: dataMember1.getTransactionByMember.transactionId,
+        completed: true,
+        card: "xyz001",
+      },
+    });
+  };
+
+  const memberTwoAccept = () => {
+    updateTransaction({
+      variables: {
+        transactionId: dataMember2.getTransactionByMember.transactionId,
+        completed: true,
+        card: "xyz002",
+      },
+    });
+  };
+
+  return (
+    <>
+      {/* Leader section */}
+      <div>
+        <div>[CURRENT TRANSACTIONS]</div>
+        <div>Leader | Member | Amount | Completed | Card</div>
+        <br />
+        {payments.map((transaction, index) => (
+          <div key={index}>
+            {transaction.leader} | {transaction.member} | {transaction.amount} |{" "}
+            {transaction.completed.toString()} | {transaction.card}
+          </div>
+        ))}
+      </div>
+      <br />
+
+      <button onClick={() => initTransaction()}>Initiate transaction</button>
+      <button onClick={() => addMemberOne()}>Add '{TEST_MEMBER_ID_ONE}'</button>
+      <button onClick={() => addMemberTwo()}>Add '{TEST_MEMBER_ID_TWO}'</button>
+      <br />
+      <br />
+
+      {numPaymentsComplete == 3 && (
+        <button onClick={() => alert("Done!")}>Submit Payments!</button>
+      )}
+
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+
+      {/* Member sections */}
+      {loadingMember1 ? (
+        <div>No active transactions for Member: '{TEST_MEMBER_ID_ONE}'</div>
+      ) : payments.includes(
+          memberLookupInPayments[dataMember1.getTransactionByMember.member]
+        ) &&
+        payments[
+          memberLookupInPayments[dataMember1.getTransactionByMember.member]
+        ].completed ? (
+        <div>Payment completed for Member: '{TEST_MEMBER_ID_ONE}'</div>
+      ) : (
+        <div>
+          <div>
+            Incoming transaction from: '
+            {dataMember1.getTransactionByMember.leader}'
+          </div>
+          <button onClick={() => memberOneAccept()}>Accept</button>
+        </div>
+      )}
+
+      <br />
+      <br />
+
+      {loadingMember2 ? (
+        <div>No active transactions for Member: '{TEST_MEMBER_ID_TWO}'</div>
+      ) : payments.includes(
+          memberLookupInPayments[dataMember2.getTransactionByMember.member]
+        ) &&
+        payments[
+          memberLookupInPayments[dataMember2.getTransactionByMember.member]
+        ].completed ? (
+        <div>Payment completed for Member: '{TEST_MEMBER_ID_TWO}'</div>
+      ) : (
+        <div>
+          <div>
+            Incoming transaction from: '
+            {dataMember2.getTransactionByMember.leader}'
+          </div>
+          <button onClick={() => memberTwoAccept()}>Accept</button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default App;
--- dough_divider_server/dough_divider_api/schema.py
+++ dough_divider_server/dough_divider_api/schema.py
@@ -9,27 +9,44 @@ from starlette.applications import Starlette
 from .models import Transaction
 from asgiref.sync import sync_to_async
 import asyncio
+import json
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 
-broadcast = Broadcast("memory://")
+pubsub = Broadcast("memory://")
+queues = []
 
 type_defs = """
     type Query {
         getAllTransactions: [TransactionWithId]!
+        getAllUsers: [User]!
     }
 
     type Mutation {
         addTransaction(input: TransactionInput!): TransactionPayload
         updateTransaction(transactionId: ID!, input: TransactionUpdateInput!): TransactionPayload
         deleteTransaction(transactionId: ID!): TransactionPayload
+        deleteAllTransactions: Boolean!
+        addUser(username: String!, email: String!, password: String!, firstName: String!, lastName: String!): User
+        login(username: String!, password: String!): Boolean
+        changePassword(username: String!, newPassword: String!): User
     }
 
     type Subscription {
       getTransactionByLeader(leader: String!): TransactionWithId!
-      getTransactionByMember(member: String!): TransactionPayload
+      getTransactionByMember(member: String!): TransactionWithId!
+    }
+
+    type User {
+      email: String!
+      password: String!
+      first_name: String!
+      last_name: String!
+      username: String!
     }
 
     type Transaction {
@@ -68,6 +85,11 @@ type_defs = """
     type TransactionPayload {
         transaction: TransactionWithId
     }
+
+    type Message {
+      sender: String
+      message: String
+    }
     """
 
 query = QueryType()
@@ -76,9 +98,14 @@ def get_all_transactions(*_):
     transactions = Transaction.objects.all()
     return transactions
 
+@query.field("getAllUsers")
+def get_all_users(*_):
+    users = User.objects.all()
+    return users
+
 mutation = MutationType()
 @mutation.field("addTransaction")
-def add_transaction(_, info, input):
+async def add_transaction(_, info, input):
     transaction = Transaction(
         leader=input["leader"],
         member=input["member"],
@@ -88,18 +115,72 @@ def add_transaction(_, info, input):
         card=input["card"]
         )
     transaction.save()
+
+    transactionJson = parseTransactionToJson(transaction)
+
+    for queue in queues:
+      await queue.put({
+        "type": "addTransaction",
+        "transaction": transactionJson
+      })
+
     return {
         "transaction": transaction
     }
 
+@mutation.field("deleteAllTransactions")
+def delete_all_transactions(_, info):
+  Transaction.objects.all().delete()
+  return True
+
+@mutation.field("addUser")
+def add_user(_, info, username, email, password, firstName, lastName):
+   user = User.objects.create_user(email=email, username=username, password=password, first_name=firstName, last_name=lastName)
+   user.save()
+   return user
+
+@mutation.field("login")
+def resolve_login(_, info, username, password):
+    if authenticate(username=username, password=password):
+       return True
+    return False
+
+@mutation.field("changePassword")
+def change_password(_, info, username, newPassword):
+    user = User.objects.get(username=username)
+    user.set_password(newPassword)
+    user.save()
+    return user
+
+# TODO
+def parseTransactionToJson(transactionObj):
+  return {
+    "transactionId": transactionObj.transactionId,
+    "leader": transactionObj.leader,
+    "member": transactionObj.member,
+    "amount": transactionObj.amount,
+    "completed": transactionObj.completed,
+    "note": transactionObj.note,
+    "card": transactionObj.card
+  }
+
 @mutation.field("updateTransaction")
-def update_transaction(_, info, transactionId, input):
+async def update_transaction(_, info, transactionId, input):
     transaction = Transaction.objects.get(transactionId=transactionId)
     transaction.card = input["card"]
     transaction.completed = input["completed"]
     transaction.save()
+
+    transactionJson = parseTransactionToJson(transaction)
+
+    for queue in queues:
+      await queue.put({
+        "type": "updateTransaction",
+        "transaction": transactionJson
+      })
+
     return {
-        "transaction": transaction
+      "transaction": transaction
     }
 
 @mutation.field("deleteTransaction")
@@ -118,21 +199,39 @@ subscription = SubscriptionType()
 
 @subscription.source("getTransactionByLeader")
 async def generate_transaction_by_leader(_, info, leader):
-  print("\nLEADER IS", leader, "\n")
+  queue = asyncio.Queue()
+  queues.append(queue)
 
-  # return Transaction.objects.filter(leader=leader)
-  async for transaction in Transaction.objects.filter(leader=leader):
-    await asyncio.sleep(1)
-    yield transaction
+  while True:
+    result = await queue.get()
+    type = result["type"]
+    transactionJson = result["transaction"]
+    queue.task_done()
+
+    if type == "updateTransaction" and leader == transactionJson["leader"]:
+      yield transactionJson
 
 @subscription.field("getTransactionByLeader")
-def resolve_transaction_by_leader(transaction, info, leader):
-  print("UPDATE TO TRANSACTIONS")
+def resolve_transaction_by_leader(transactionJson, info, leader):
+  return transactionJson
 
-  print(transaction)
-  print(leader)
+@subscription.source("getTransactionByMember")
+async def generate_transaction_by_member(_, info, member):
+  queue = asyncio.Queue()
+  queues.append(queue)
 
-  return transaction
+  while True:
+    result = await queue.get()
+    type = result["type"]
+    transactionJson = result["transaction"]
+    queue.task_done()
+
+    if type == "addTransaction" and member == transactionJson["member"] and transactionJson["leader"] != transactionJson["member"]:
+      yield transactionJson
+
+@subscription.field("getTransactionByMember")
+def resolve_transaction_by_member(transactionJson, info, member):
+  return transactionJson
 
 schema = make_executable_schema([type_defs], [query, mutation, subscription])
 graphql = GraphQL(
@@ -143,8 +242,8 @@ graphql = GraphQL(
 
 app = Starlette(
     debug=True,
-    on_startup=[broadcast.connect],
-    on_shutdown=[broadcast.disconnect],
+    on_startup=[pubsub.connect],
+    on_shutdown=[pubsub.disconnect],
 )
 
 app.mount("/", graphql)
